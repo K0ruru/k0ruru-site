ae5d1cd67cfce0f8b26262df1ae6e71c01bd5f80
K0ruru <nurfaizqalbi@gmail.com>
2024-02-25 23:51:25 +0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,15 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-	const handleScroll = (event) => {
-		console.log(event.target);
-		const container = event.currentTarget;
-		const scrollAmount = event.deltaY;
-
-		container.scrollLeft += scrollAmount;
-	};
-
 	return (
-		<div className="app-container" onWheel={handleScroll}>
+		<>
 			<Navbar />
-			<div className="horizontal-scroll-container">
-				<div className="page-container">
-					<Home className="component" />
-				</div>
-				<div className="page-container">
-					<About className="component" />
-				</div>
-				{/* Add more page containers as needed */}
-			</div>
-		</div>
+			<Home />
+			<About />
+		</>
 	);
 }
 
